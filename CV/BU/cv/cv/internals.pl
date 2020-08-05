32204f55c16eafcadd274ee4803d57b5819e858e
elioa <elio.farina@gmail.com>
2020-08-05 19:42:55 +0100

    CV

---
+++ CV/BU/cv/cv/internals.pl
@@ -0,0 +1,10 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate internals original text with physical files.
+
+
+$key = q/ultimapagina/;
+$ref_files{$key} = "$dir".q|cv.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
